034d48404ecc7f88e176fb04ab92f4ad9d0d524e
Oleg Chibikov <ochibikov@gmail.com>
2023-08-26 14:24:39 +1000

    Fix invalid upscaled url

--- src/backend/helpers/uploadHelper.ts
+++ src/backend/helpers/uploadHelper.ts
@@ -2,8 +2,8 @@ import path from 'path';
 
 const outputDirectory = 'public';
 const outputPath = path.join(path.resolve(process.cwd()), outputDirectory);
-const toWebUrl = (path: string) => '/' + path.replace(`${outputPath}\\`, '');
+const toWebUrl = (path: string) => path.replace(`${outputPath}\\`, '');
 
 const toServerUrl = (path: string) => `${outputPath}\\${path.substring(1)}`;
 
-export { toWebUrl, toServerUrl, outputPath };
+export { outputPath, toServerUrl, toWebUrl };
