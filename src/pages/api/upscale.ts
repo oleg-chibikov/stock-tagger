428c3eee8d950a5d0818d79871bd9328531f0806
Oleg Chibikov <ochibikov@gmail.com>
2023-08-27 23:49:25 +1000

    refactoring

--- src/pages/api/upscale.ts
+++ src/pages/api/upscale.ts
@@ -1,4 +1,7 @@
-import { withMultiOperationsCancellation } from '@appHelpers/cancellationToken';
+import {
+  CancellationToken,
+  withMultiOperationsCancellation,
+} from '@appHelpers/cancellationToken';
 import { apiHandler } from '@backendHelpers/apiHelper';
 import {
   convertToImageFileData,
@@ -8,6 +11,7 @@ import {
 import { UpscaleModel } from '@dataTransferTypes/upscaleModel';
 import { SftpService } from '@services/sftpService';
 import { UpscalerService } from '@services/upscalerService';
+import { File } from 'formidable';
 import { NextApiRequest, NextApiResponse } from 'next';
 import Queue from 'queue-promise';
 import Container from 'typedi';
@@ -27,46 +31,57 @@ const upscale = async (req: NextApiRequest, res: NextApiResponse) => {
     const upscaleQueue = new Queue({
       concurrent: 1,
     });
+
     const uploadQueue = uploadImmediately
       ? new Queue({
           concurrent: 1,
         })
       : undefined;
-    await withMultiOperationsCancellation(
-      new Set(uploadImmediately ? ['upscale', 'ftp_upload'] : ['upscale']),
-      async (cancellationToken) =>
-        await new Promise(async (resolve, reject) => {
-          if (uploadImmediately) {
-            cancellationToken.addCancellationCallback(async () => {
-              await sftpService.disconnect(cancellationToken);
-            });
-            uploadQueue?.enqueue(() =>
-              sftpService.connectIfNeeded(cancellationToken)
-            );
-          }
-          const images = getFilesFromRequest(files);
-          console.log(`Processing ${images.length} images...`);
-          let upscaleFinished = false;
-          let uploadFinished = !uploadImmediately;
-          const upscaleTasks = images.map((image) => async () => {
+
+    const processImages = async (
+      cancellationToken: CancellationToken
+    ): Promise<void> =>
+      new Promise<void>(async (resolve, reject) => {
+        if (uploadImmediately) {
+          cancellationToken.addCancellationCallback(async () => {
+            await sftpService.disconnect(cancellationToken);
+          });
+          uploadQueue?.enqueue(() =>
+            sftpService.connectIfNeeded(cancellationToken)
+          );
+        }
+        const images = getFilesFromRequest(files);
+        console.log(`Processing ${images.length} images...`);
+        let upscaleFinished = false;
+        let uploadFinished = !uploadImmediately;
+
+        const processImage =
+          (image: File): (() => Promise<void>) =>
+          async () => {
             if (cancellationToken.isCancellationRequested) {
               reject();
               return Promise.reject();
             }
             const convertedImage = convertToImageFileData(image);
-            const upscaledFilePath = await upscalerService.upscaleWithEvents(
-              io,
-              convertedImage,
-              image.newFilename,
-              fields.modelName as unknown as UpscaleModel,
-              cancellationToken,
-              0,
-              uploadImmediately ? 0.5 : 1
-            );
+
+            const upscaleImage = async () =>
+              upscalerService.upscaleWithEvents(
+                io,
+                convertedImage,
+                image.newFilename,
+                fields.modelName as unknown as UpscaleModel,
+                cancellationToken,
+                0,
+                uploadImmediately ? 0.5 : 1
+              );
+
+            const upscaledFilePath = await upscaleImage();
+
             if (upscaledFilePath && uploadImmediately) {
               convertedImage.filePath = upscaledFilePath;
               uploadFinished = false;
-              uploadQueue?.enqueue(() => {
+
+              const uploadImageToSftp = () => {
                 if (cancellationToken.isCancellationRequested) {
                   reject();
                   return Promise.reject();
@@ -77,25 +92,35 @@ const upscale = async (req: NextApiRequest, res: NextApiResponse) => {
                   cancellationToken,
                   0.5
                 );
-              });
-            }
-          });
-          const resolveWhenDone = () => {
-            if (uploadFinished && upscaleFinished) {
-              resolve();
+              };
+
+              uploadQueue?.enqueue(uploadImageToSftp);
             }
           };
-          upscaleQueue.enqueue(upscaleTasks);
-          uploadQueue?.on('end', () => {
-            uploadFinished = true;
-            resolveWhenDone();
-          });
 
-          upscaleQueue.on('end', () => {
-            upscaleFinished = true;
-            resolveWhenDone();
-          });
-        })
+        const upscaleTasks = images.map(processImage);
+
+        const resolveWhenDone = () => {
+          if (uploadFinished && upscaleFinished) {
+            resolve();
+          }
+        };
+
+        upscaleQueue.enqueue(upscaleTasks);
+        uploadQueue?.on('end', () => {
+          uploadFinished = true;
+          resolveWhenDone();
+        });
+
+        upscaleQueue.on('end', () => {
+          upscaleFinished = true;
+          resolveWhenDone();
+        });
+      });
+
+    await withMultiOperationsCancellation(
+      new Set(uploadImmediately ? ['upscale', 'ftp_upload'] : ['upscale']),
+      processImages
     );
   });
 };
